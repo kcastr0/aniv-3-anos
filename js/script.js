97943565c301ff7c42920ca7965520c84eb4b607
Kamilla Castro <103784478+kcastr0@users.noreply.github.com>
2024-12-16 23:53:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 const thumbnails = document.querySelectorAll('.thumbnail');
 const mainImage = document.getElementById('main-image');
 
-// Criando a função de exibição ampliada
+// Adicionando o evento de clique para cada miniatura
 thumbnails.forEach((thumbnail) => {
     thumbnail.addEventListener('click', () => {
-        // Atualizando a imagem principal com a imagem clicada
+        // Atualizando a imagem principal com a imagem da miniatura clicada
         mainImage.src = thumbnail.src;
+
+        // Remover a classe "active" de todas as miniaturas
+        thumbnails.forEach((thumb) => thumb.classList.remove('active'));
+
+        // Adicionar a classe "active" na miniatura clicada
+        thumbnail.classList.add('active');
     });
 });
