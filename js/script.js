1b5cf7d35ad58e5fbb7013e2ba0ddfd323c273bf
Kamilla Castro <103784478+kcastr0@users.noreply.github.com>
2024-12-16 23:46:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-// Função para trocar a imagem principal quando a miniatura for clicada
-const thumbnails = document.querySelectorAll('.thumbnail');
-const mainImage = document.getElementById('main-image');
-
-thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', () => {
-        mainImage.src = thumbnail.src;
-    });
-});
+// Função para alterar a imagem principal
+function changeImage(imagePath) {
+    const mainImage = document.getElementById('main-image');
+    mainImage.src = imagePath;
+}
