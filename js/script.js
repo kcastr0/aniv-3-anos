d53c6c32b65851d8fa2b931a3acff30dac32f40d
Kamilla Castro <103784478+kcastr0@users.noreply.github.com>
2024-12-16 23:57:16 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@
 const thumbnails = document.querySelectorAll('.thumbnail');
 const mainImage = document.getElementById('main-image');
 
+// Função que altera a imagem principal
+function changeMainImage(event) {
+    // Atualizando a imagem principal com a imagem da miniatura clicada
+    const newImage = event.target.getAttribute('data-image');
+    mainImage.src = `images/${newImage}`;
+
+    // Remover a classe 'active' de todas as miniaturas
+    thumbnails.forEach((thumb) => thumb.classList.remove('active'));
+
+    // Adicionar a classe 'active' na miniatura clicada
+    event.target.classList.add('active');
+}
+
 // Adicionando o evento de clique para cada miniatura
 thumbnails.forEach((thumbnail) => {
-    thumbnail.addEventListener('click', () => {
-        // Atualizando a imagem principal com a imagem da miniatura clicada
-        mainImage.src = thumbnail.src;
-
-        // Remover a classe "active" de todas as miniaturas
-        thumbnails.forEach((thumb) => thumb.classList.remove('active'));
-
-        // Adicionar a classe "active" na miniatura clicada
-        thumbnail.classList.add('active');
-    });
+    thumbnail.addEventListener('click', changeMainImage);
 });
